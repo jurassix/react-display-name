4af8e25e727b47d7829c1df847c9951cdfc542af
Clint Ayres <clinton.ayres@gmail.com>
2017-07-31 14:10:16 -0400

    Fix #8, #9, and make progress on #5

--- src/getDisplayName.js
+++ src/getDisplayName.js
@@ -1,7 +1,9 @@
-const getDisplayName = Component => (
-  Component.displayName ||
-  Component.name ||
-  (typeof Component === 'string' ? Component : 'Component')
-);
-
-export default getDisplayName;
+export default function getDisplayName(Component) {
+  return (
+    Component.displayName ||
+    Component.name ||
+    (typeof Component === 'string' && Component.length > 0
+      ? Component
+      : 'Unknown')
+  );
+}
